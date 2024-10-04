86bff204b97f77954f70f7b9e1fd32f7e17a8502
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-03 21:25:19 -0300

    Produto Repository

---
+++ web-app-performance/Controllers/UsuarioController.cs
@@ -0,0 +1,83 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using web_app_domain;
+using web_app_repository;
+
+namespace web_app_performance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private static ConnectionMultiplexer redis;
+        private readonly IUsuarioRepository _repository;
+
+        public UsuarioController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsuario()
+        {
+            string key = "getUsuarios";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyExpireAsync(key, TimeSpan.FromSeconds(10));
+            string user = await db.StringGetAsync(key);
+
+            if(!string.IsNullOrEmpty(user))
+            {
+                return Ok(user);
+            }
+
+            var usuarios = await _repository.ListarUsuarios();
+            string usuariosJson = JsonConvert.SerializeObject(usuarios);
+            await db.StringSetAsync(key, usuariosJson);
+
+            return Ok(usuarios);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Usuario usuario)
+        {
+            await _repository.SalvarUsuario(usuario);
+
+            string key = "getUsuarios";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Usuario usuario)
+        {
+            await _repository.AtualizarUsuario(usuario);
+
+            string key = "getUsuarios";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repository.RemoverUsuario(id);
+
+            string key = "getUsuarios";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+    }
+}
